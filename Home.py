42ae0071d3ba503ae13411f5c9a5d880a6ee5a38
u-genuine <u_genuine@naver.com>
2026-01-22 14:46:31 +0900

    Streamlit 기본 구조 학습 - 채팅 UI & session_create
    - st.chat_message, st.chat_input으로 채팅 UI 구현
    - session_state로 대화 내역 유지 및 복원
    - send_message 함수로 메시지 표시 및 저장 로직 분리

---
+++ Home.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title= "FullstackGPT Home",
+    page_icon= "🤖"
+)
+
+st.title("FullstackGPT Home")
+
+with st.sidebar:
+    st.title("sidebar title")
+    st.text_input("xxx")
+
+tab_one, tab_two, tab_three = st.tabs(["A", "B", "C"])
+
+with tab_one:
+    st.write('a')
+
+with tab_two:
+    st.write('b')
+
+with tab_three:
+    st.write('c')
+
+"""
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeeingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+
+"""
