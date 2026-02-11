7e15b6447775d7af34972df34a4b521b11066435
u-genuine <u_genuine@naver.com>
2026-02-11 13:27:22 +0900

    Output parser을 Function Calling으로 대체

--- pages/02_QuizGPT.py
+++ pages/02_QuizGPT.py
@@ -1,4 +1,3 @@
-from regex import T
 import streamlit as st
 import json
 from langchain.retrievers import WikipediaRetriever
@@ -7,16 +6,8 @@ from langchain.document_loaders import UnstructuredFileLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.callbacks import StreamingStdOutCallbackHandler
-from langchain.schema import BaseOutputParser
 from utils import format_docs
 from pathlib import Path
-
-class JsonOutputParser(BaseOutputParser):
-    def parse(self, text):
-        text = text.replace("```", "").replace("json", "")
-        return json.loads(text)
-    
-output_parser = JsonOutputParser()
 
 
 # 스트림릿 페이지 설정
@@ -27,11 +18,47 @@ st.set_page_config(
 
 st.title("QuizGPT")
 
+# LLM에게 알려줄 함수의 명세서 (인터페이스 정의)
+function = {
+    "name": "create_quiz",
+    "description": "질문과 답변 목록을 가진 퀴즈를 반환하는 함수",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "questions": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "question": {"type": "string"},
+                        "answers": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "answer": {"type": "string"},
+                                    "correct": {"type": "boolean"}
+                                },
+                                "required": ["answer", "correct"]
+                            }
+                        }
+                    },
+                    "required": ["question", "answers"]
+                }
+            },
+        },
+        "required": ["questions"]
+    },
+}
+
 llm = ChatOpenAI(
     temperature=1.0,
-    model="gpt-5-nano",
+    model="gpt-4.1-nano",
     streaming=True,
     callbacks=[StreamingStdOutCallbackHandler()] # 터미널에 실시간 답변 출력
+).bind(
+    function_call = {"name": "create_quiz"},
+    functions = [function]
 )
 
 # 문서에서 퀴즈 텍스트를 생성하는 프롬프트
@@ -44,113 +71,16 @@ questions_prompt = ChatPromptTemplate.from_messages(
             오직 아래 제공된 Context만을 바탕으로 사용자의 지식을 테스트하기 위한 퀴즈 10개를 만드세요.
 
             각 문제는 4개의 선택지를 가져야 하며, 그중 3개는 틀린 답이고 1개만 정답이어야 합니다.
-
-            정답인 선택지 뒤에는 반드시 (✅) 표시를 하세요.
-            
-            질문 예시: 
-            
-            질문: 바다의 색깔은 무엇인가요?
-            선택지: 빨강 | 노랑 | 초록 | 파랑(✅)
-
-            질문: 조지아의 수도는 어디인가요?
-            선택지: 바쿠 | 트빌리시(✅) | 마닐라 | 베이루트
-            
-            질문: 영화 '아바타'는 언제 개봉했나요?
-            선택지: 2007 | 2001 | 2009(✅) | 1998
-            
-            질문: Julius Caesar는 누구인가요?
-            선택지: 로마의 황제(✅) | 화가 | 배우 | 모델
-            
-            당신의 차례입니다!
             
             Context: {context}
             """
-            )
-        ]
-    )
-
-# 문서 포맷팅 -> 퀴즈 생성 체인
-questions_chain = {"context": format_docs} | questions_prompt | llm
-
-# 텍스트를 JSON 구조로 변환하는 프롬프트
-# {{ }} 중괄호 두 번 사용은 LangChain 변수 치환 방지
-formatting_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-            당신은 강력한 포맷팅 알고리즘입니다.
-
-            당신은 시험 문제들을 전달받아 JSON 형식으로 변환합니다.
-            선택지 중 (✅) 표시가 있는 것이 정답입니다.
-
-            입력 예시:    
-
-            질문: 바다의 색깔은 무엇인가요?
-            선택지: 빨강 | 노랑 | 초록 | 파랑(✅)
-
-            질문: 조지아의 수도는 어디인가요?
-            선택지: 바쿠 | 트빌리시(✅) | 마닐라 | 베이루트
-            
-            질문: 영화 '아바타'는 언제 개봉했나요?
-            선택지: 2007 | 2001 | 2009(✅) | 1998
-            
-            질문: Julius Caesar는 누구인가요?
-            선택지: 로마의 황제(✅) | 화가 | 배우 | 모델
-
-            출력 예시:
-            
-            ```json
-            {{ "questions": [
-                {{
-                    "question": "바다의 색깔은 무엇인가요?",
-                    "answers": [
-                            {{ "answer": "빨강", "correct": false }},
-                            {{ "answer": "노랑", "correct": false }},
-                            {{ "answer": "초록", "correct": false }},
-                            {{ "answer": "파랑", "correct": true }}
-                    ]
-                }},
-                {{
-                    "question": "조지아의 수도는 어디인가요?",
-                    "answers": [
-                            {{ "answer": "바쿠", "correct": false }},
-                            {{ "answer": "트빌리시", "correct": true }},
-                            {{ "answer": "마닐라", "correct": false }},
-                            {{ "answer": "베이루트", "correct": false }}
-                    ]
-                }},
-                {{
-                    "question": "영화 '아바타'는 언제 개봉했나요?",
-                    "answers": [
-                            {{ "answer": "2007", "correct": false }},
-                            {{ "answer": "2001", "correct": false }},
-                            {{ "answer": "2009", "correct": true }},
-                            {{ "answer": "1998", "correct": false }}
-                    ]
-                }},
-                {{
-                    "question": "율리우스 카이사르(Julius Caesar)는 누구인가요?",
-                    "answers": [
-                            {{ "answer": "로마의 황제", "correct": true }},
-                            {{ "answer": "화가", "correct": false }},
-                            {{ "answer": "배우", "correct": false }},
-                            {{ "answer": "모델", "correct": false }}
-                    ]
-                }}
-            ]
-            }}
-            ```
-            당신 차례입니다!
-
-            질문 목록: {context}
-        """
         )
     ]
 )
 
-# 퀴즈 JSON 변환 체인
-formatting_chain = formatting_prompt | llm
+# 문서 포맷팅 -> 퀴즈 생성 체인
+questions_chain = {"context": format_docs} | questions_prompt | llm
+
 
 @st.cache_data(show_spinner = "Loading file...")
 def split_file(file):
@@ -179,8 +109,12 @@ def split_file(file):
 def run_quiz_chain(_docs, topic):
     # _docs: docs는 해싱 제외
     # topic: 대신 파일명이나 검색어가 바뀐지 보고 재실행 여부 결정
-    chain = {"context": questions_chain} | formatting_chain | output_parser
-    return chain.invoke(_docs)
+    response = questions_chain.invoke(_docs)
+    # | formatting_chain | output_parser
+
+    arguments = response.additional_kwargs["function_call"]["arguments"]
+
+    return json.loads(arguments)
 
 
 @st.cache_data(show_spinner="Searching Wikipedia...")
